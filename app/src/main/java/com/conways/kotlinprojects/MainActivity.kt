393b955532261310d15d15baf8e66c0c812f1e2c
zongshili <congways@163.com>
2018-10-17 11:04:35 +0800

    修复被回收时导致层级错乱的问题

--- app/src/main/java/com/conways/kotlinprojects/MainActivity.kt
+++ app/src/main/java/com/conways/kotlinprojects/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.conways.kotlinprojects
 
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -12,18 +14,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         private var INDEX_FRUIT = 0
         private var INDEX_MEAT = 1
         private var INDEX_VEGETABLES = 2
+
+        private var INDEX_FRUIT_TAG = "fruit"
+        private var INDEX_MEAT_TAG = "meat"
+        private var INDEX_VEGETABLES_TAG = "vegetables"
+        private var CORRENT_INDEX = "corrent_index"
     }
 
     var fruitFragment: FruitFragment? = null
     var meatFragment: MeatFragment? = null
     var vegetablesFragment: VegetablesFragment? = null
 
+    var index: Int? = INDEX_FRUIT
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
-        setIndex(INDEX_FRUIT)
+        if (null != savedInstanceState) {
+            index = savedInstanceState.getInt(CORRENT_INDEX)
+            fruitFragment = supportFragmentManager.findFragmentByTag(INDEX_FRUIT_TAG) as FruitFragment?
+            meatFragment = supportFragmentManager.findFragmentByTag(INDEX_MEAT_TAG) as MeatFragment?
+            vegetablesFragment = supportFragmentManager.findFragmentByTag(INDEX_VEGETABLES_TAG) as VegetablesFragment?
+        }
+        setIndex(this.index!!)
     }
 
     fun init() {
@@ -35,9 +50,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.activity_main_index_fruit -> setIndex(INDEX_FRUIT)
-            R.id.activity_main_index_meat -> setIndex(INDEX_MEAT)
-            R.id.activity_main_index_vegetables -> setIndex(INDEX_VEGETABLES)
+            R.id.activity_main_index_fruit -> {
+                if (this.index != INDEX_FRUIT) {
+                    setIndex(INDEX_FRUIT)
+                }
+            }
+
+            R.id.activity_main_index_meat -> {
+                if (this.index != INDEX_MEAT) {
+                    setIndex(INDEX_MEAT)
+                }
+            }
+            R.id.activity_main_index_vegetables -> {
+                if (this.index != INDEX_VEGETABLES) {
+                    setIndex(INDEX_VEGETABLES)
+                }
+            }
             else -> {
 
             }
@@ -52,34 +80,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             INDEX_FRUIT -> {
                 if (null == fruitFragment) {
                     fruitFragment = FruitFragment();
-                    fragmentTransaction.add(R.id.activity_main_fragment_holder, fruitFragment!!, "fruit")
+                    fragmentTransaction.add(R.id.activity_main_fragment_holder, fruitFragment!!, INDEX_FRUIT_TAG)
                 } else {
                     fragmentTransaction.show(fruitFragment!!)
                 }
                 activity_main_index_fruit?.setCompoundDrawablesWithIntrinsicBounds(null, getDrawable(R.drawable.ic_fruit_checked), null, null)
+                activity_main_index_fruit?.setTextColor(resources.getColor(R.color.colorMain))
             }
 
             INDEX_MEAT -> {
                 if (null == meatFragment) {
                     meatFragment = MeatFragment();
-                    fragmentTransaction.add(R.id.activity_main_fragment_holder, meatFragment!!, "meat")
+                    fragmentTransaction.add(R.id.activity_main_fragment_holder, meatFragment!!, INDEX_MEAT_TAG)
                 } else {
                     fragmentTransaction.show(meatFragment!!)
                 }
                 activity_main_index_meat?.setCompoundDrawablesWithIntrinsicBounds(null, getDrawable(R.drawable.ic_meat_checked), null, null)
+                activity_main_index_meat?.setTextColor(resources.getColor(R.color.colorMain))
             }
 
             INDEX_VEGETABLES -> {
                 if (null == vegetablesFragment) {
                     vegetablesFragment = VegetablesFragment();
-                    fragmentTransaction.add(R.id.activity_main_fragment_holder, vegetablesFragment!!, "vegetables")
+                    fragmentTransaction.add(R.id.activity_main_fragment_holder, vegetablesFragment!!, INDEX_VEGETABLES_TAG)
                 } else {
                     fragmentTransaction.show(vegetablesFragment!!)
                 }
                 activity_main_index_vegetables?.setCompoundDrawablesWithIntrinsicBounds(null, getDrawable(R.drawable.ic_vegetables_checked), null, null)
+                activity_main_index_vegetables?.setTextColor(resources.getColor(R.color.colorMain))
+
             }
         }
         fragmentTransaction.commit()
+        this.index = index;
 
     }
 
@@ -100,5 +133,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         activity_main_index_fruit?.setCompoundDrawablesWithIntrinsicBounds(null, getDrawable(R.drawable.ic_fruit_normal), null, null)
         activity_main_index_meat?.setCompoundDrawablesWithIntrinsicBounds(null, getDrawable(R.drawable.ic_meat_normal), null, null)
         activity_main_index_vegetables?.setCompoundDrawablesWithIntrinsicBounds(null, getDrawable(R.drawable.ic_vegetables_normal), null, null)
+
+        activity_main_index_fruit?.setTextColor(resources.getColor(R.color.colorGrayDark))
+        activity_main_index_meat?.setTextColor(resources.getColor(R.color.colorGrayDark))
+        activity_main_index_vegetables?.setTextColor(resources.getColor(R.color.colorGrayDark))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        if (outState != null) {
+            outState.putInt(CORRENT_INDEX, this.index!!)
+        }
     }
 }
